5be6b3bd7356d1ecc1453d0197328e4258f0918e
Samu <shaibusamuel423@gmail.com>
2024-11-12 06:12:45 +0100

    omg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import backgroundImage from '../assets/blueglitterbackground.jpg'; // Update path to your image
 
 const Home = () => {
   return (
-    <div className="text-center">
-      <h1>We deliver IT solutions that fit your business</h1>
-      <p>At EDSINE Technologies, we empower businesses with innovative technology solutions designed to drive success.</p>
+    <div className="relative w-full h-screen">
+      {/* Background image */}
+      <img 
+        src={backgroundImage} 
+        alt="Background" 
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+      {/* Text container */}
+      <div className="relative z-10 flex flex-col justify-center items-start pl-16 h-full w-full bg-black bg-opacity-50">
+        {/* Large, left-aligned heading */}
+        <h1 className="text-white text-7xl font-bold mb-4">We deliver IT solutions <p></p>that fit your business</h1>
+        {/* Left-aligned paragraph */}
+        <p className="text-white">
+          Empowering Businesses With Innovative Tech Solutions
+        </p>
+      </div>
     </div>
   );
 };
